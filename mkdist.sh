a98ef0aef29112e622ba7598f9f5fe88dca6eff6
agauchy <antho2gauchy@gmail.com>
2017-02-14 14:53:27 +0100

    Ajout log, creation du fichier md

--- mkdist.sh
+++ mkdist.sh
@@ -21,7 +21,7 @@ DISK=minidist.img
 # First of all, do compile the hello program that will be used
 # as the init process when booting just a bare kernel, without 
 # any distribution:
-
+echo "Make"
 (cd hello ; make -s )
 cp hello/hello MiniDist/
 cp hello/initrd.hello MiniDist/boot/initrd.hello
@@ -29,19 +29,20 @@ cp hello/initrd.hello MiniDist/boot/initrd.hello
 # Create the raw image...
 # with the GRUB stage1 and stage2 at the beginning.
 # and some room after to later create a file system
-
+echo "DD"
 dd if=$GRUB_DIR/lib/grub/i386-pc/stage1 of=$DISK bs=512 count=1
 dd if=$GRUB_DIR/lib/grub/i386-pc/stage2 of=$DISK bs=512 seek=1
 
 # we want to create a partition now, starting at sector 256,
 # using 128,000 sectors, which is about 64MB.
-
+echo "DD2"
 dd if=/dev/zero of=$DISK bs=512 count=128000 seek=256
 
 # but the disk label in the GRUB boot sector is not recognized 
 # by parted... so let's use fdisk to cleanup things a bit first.
 # Use fdisk to delete all partitions and rewrite the partition table,
 # which seems to cleanup the disk label for parted to work.
+echo "fdisk"
 fdisk $DISK <<EOF
 d 
 4
@@ -59,6 +60,7 @@ EOF
 # but also because we can control much better the start of 
 # partition with parted, which is crucial for the rest of this script.
 # Note that we start the partition at sector 256.
+echo "Parted"
 parted -s $DISK mkpart primary ext2 256s 128000s
 
 # now we need to have a block device for that partition.
@@ -70,7 +72,7 @@ parted -s $DISK mkpart primary ext2 256s 128000s
 # Note: 256*512=131072
 # Could be written
 #     sudo losetup -o$((256*512)) /dev/loop1 $DISK
-
+echo "Losetup mke2fs"
 sudo mkdir -p $MOUNT_DIR
 sudo losetup -o131072 /dev/loop1 $DISK
 sudo mke2fs /dev/loop1
@@ -80,6 +82,7 @@ sudo mke2fs /dev/loop1
 # there are two ways to do it in Linux.
 # The preferred way is to use the loop device we just created:
 # mkdir -p $MOUNT_DIR
+echo "Mount"
 sudo mount /dev/loop1 $MOUNT_DIR
 
 # It is possible however to mount a file at an offset directly
@@ -96,9 +99,11 @@ sudo mount /dev/loop1 $MOUNT_DIR
 ( cd $DIST_DIR ; sudo cp -ar . $MOUNT_DIR )
 
 # Sync the file system buffers with the underlying devices:
+echo "Sync"
 sync
 
 # Unmount our partition and free the loop device we created
+echo "Unmount losetup"
 sudo umount /dev/loop1
 sudo losetup -d /dev/loop1
 
